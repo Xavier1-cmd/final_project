af6dda07e0c9adc42d7ceaf1ea6591925e5d717d
Ikaruga <jumiya001@gmail.com>
2024-07-12 11:14:13 +0800

    完成商品展示資料庫

--- index_s01.php
+++ index_s01.php
@@ -1,3 +1,29 @@
+<?php
+require_once("connMysql.php");
+//預設每頁筆數
+$pageRow_records = 8;
+//預設頁數
+$num_pages = 1;
+//若已經有翻頁，將頁數更新
+if (isset($_GET['page'])) {
+  $num_pages = $_GET['page'];
+}
+//本頁開始記錄筆數 = (頁數-1)*每頁記錄筆數
+$startRow_records = ($num_pages - 1) * $pageRow_records;
+//未加限制顯示筆數的SQL敘述句
+$query_RecAlbum = "SELECT album.album_id , album.album_date , album.album_location , album.album_title , album.album_desc , albumphoto.ap_picurl, count( albumphoto.ap_id ) AS albumNum FROM album LEFT JOIN albumphoto ON album.album_id = albumphoto.album_id GROUP BY album.album_id , album.album_date , album.album_location , album.album_title , album.album_desc ORDER BY album_date DESC";
+//加上限制顯示筆數的SQL敘述句，由本頁開始記錄筆數開始，每頁顯示預設筆數
+$query_limit_RecAlbum = $query_RecAlbum . " LIMIT {$startRow_records}, {$pageRow_records}";
+//以加上限制顯示筆數的SQL敘述句查詢資料到 $RecAlbum 中
+$RecAlbum = $db_link->query($query_limit_RecAlbum);
+//以未加上限制顯示筆數的SQL敘述句查詢資料到 $all_RecAlbum 中
+$all_RecAlbum = $db_link->query($query_RecAlbum);
+//計算總筆數
+$total_records = $all_RecAlbum->num_rows;
+//計算總頁數=(總筆數/每頁筆數)後無條件進位。
+$total_pages = ceil($total_records / $pageRow_records);
+?>
+
 <!doctype html>
 <html lang="zh-TW">
 
@@ -216,7 +242,7 @@
           </div> -->
           <hr>
           <div class="row text-center">
-            <div class="card col-md-3">
+            <!-- <div class="card col-md-3">
               <img src="./product_img/pic0101.jpg" class="card-img-top" alt="濕身阿爾維娜">
               <div class="card-body">
                 <h5 class="card-title">濕身阿爾維娜</h5>
@@ -226,7 +252,100 @@
                 <a href="#" class="btn btn-primary">更多資訊</a>
                 <a href="#" class="btn btn-success">放購物車</a>
               </div>
-            </div>
+            </div> -->
+
+            <!-- 顯示商品資訊 -->
+            <?php while ($row_RecAlbum = $RecAlbum->fetch_assoc()) { ?>
+              <div class="card col-md-3">
+                <img src="photos/<?php echo $row_RecAlbum["ap_picurl"]; ?>" alt="<?php echo $row_RecAlbum["album_title"]; ?>" />
+                <div class="card-body">
+                  <div class="card-body">
+                    <h5 class="card-title"><?php echo $row_RecAlbum["album_title"]; ?></h5>
+                    <p class="card-text"><?php echo $row_RecAlbum["album_desc"]; ?></p>
+                    <span class="card-text text-decoration-line-through">TWD $2,000</span>
+                    <p class="card-text">TWD $1,650</p>
+                    <a href="#" class="btn btn-primary">更多資訊</a>
+                    <a href="#" class="btn btn-success">放購物車</a>
+                    <!-- <a href="albumshow.php?id=<?php echo $row_RecAlbum["album_id"]; ?>">
+                      <?php if ($row_RecAlbum["albumNum"] == 0) { ?>
+                        <img src="images/nopic.png" alt="暫無圖片" width="120" height="120" border="0" />
+                      <?php } else { ?>
+                        <img src="photos/<?php echo $row_RecAlbum["ap_picurl"]; ?>" alt="<?php echo $row_RecAlbum["album_title"]; ?>" width="120" height="120" border="0" />
+                      <?php } ?>
+                    </a> -->
+                    <div class="albuminfo"><a href="albumshow.php?id=<?php echo $row_RecAlbum["album_id"]; ?>"><?php echo $row_RecAlbum["album_title"]; ?></a><br />
+                      <span class="smalltext">共 <?php echo $row_RecAlbum["albumNum"]; ?> 張</span><br>
+                      <br>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            <?php } ?>
+
+            <!-- 網路相簿程式 -->
+            <table width="778" border="0" align="center" cellpadding="0" cellspacing="0">
+              <tr>
+                <td height="124" valign="top" background="images/album_r1_c1.jpg">
+                  <div class="titleDiv"> [生活、旅行的記錄]<br />
+                  </div>
+                  <div class="menulink"><a href="index.php">[相簿首頁]</a> <a href="login.php">[相簿管理]</a></div>
+                </td>
+              </tr>
+              <tr>
+                <td background="images/album_r2_c1.jpg">
+                  <div id="mainRegion">
+                    <table width="90%" border="0" align="center" cellpadding="4" cellspacing="0">
+                      <tr>
+                        <td>
+                          <div class="subjectDiv"> 網路相簿總覽 </div>
+                          <div class="actionDiv">相簿總數: <?php echo $total_records; ?></div>
+                          <div class="normalDiv"></div>
+                          <?php while ($row_RecAlbum = $RecAlbum->fetch_assoc()) { ?>
+                            <div class="albumDiv">
+                              <div class="picDiv"><a href="albumshow.php?id=<?php echo $row_RecAlbum["album_id"]; ?>"><?php if ($row_RecAlbum["albumNum"] == 0) { ?><img src="images/nopic.png" alt="暫無圖片" width="120" height="120" border="0" /><?php } else { ?><img src="photos/<?php echo $row_RecAlbum["ap_picurl"]; ?>" alt="<?php echo $row_RecAlbum["album_title"]; ?>" width="120" height="120" border="0" /><?php } ?></a></div>
+                              <div class="albuminfo"><a href="albumshow.php?id=<?php echo $row_RecAlbum["album_id"]; ?>"><?php echo $row_RecAlbum["album_title"]; ?></a><br />
+                                <span class="smalltext">共 <?php echo $row_RecAlbum["albumNum"]; ?> 張</span><br>
+                                <br>
+                              </div>
+                            </div>
+                          <?php } ?>
+                          <div class="navDiv">
+                            <?php if ($num_pages > 1) { // 若不是第一頁則顯示 
+                            ?>
+                              <a href="?page=1">|&lt;</a> <a href="?page=<?php echo $num_pages - 1; ?>">&lt;&lt;</a>
+                            <?php } else { ?>
+                              |&lt; &lt;&lt;
+                            <?php } ?>
+                            <?php
+                            for ($i = 1; $i <= $total_pages; $i++) {
+                              if ($i == $num_pages) {
+                                echo $i . " ";
+                              } else {
+                                echo "<a href=\"?page=$i\">$i</a> ";
+                              }
+                            }
+                            ?>
+                            <?php if ($num_pages < $total_pages) { // 若不是最後一頁則顯示 
+                            ?>
+                              <a href="?page=<?php echo $num_pages + 1; ?>">&gt;&gt;</a> <a href="?page=<?php echo $total_pages; ?>">&gt;|</a>
+                            <?php } else { ?>
+                              &gt;&gt; &gt;|
+                            <?php } ?>
+                          </div>
+                        </td>
+                      </tr>
+                    </table>
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td align="center" background="images/album_r2_c1.jpg" class="trademark">© 2016 eHappy Studio All Rights Reserved.</td>
+              </tr>
+              <tr>
+                <td><img name="album_r4_c1" src="images/album_r4_c1.jpg" width="778" height="24" border="0" id="album_r4_c1" alt="" /></td>
+              </tr>
+            </table>
+
             <!-- <div class="card col-md-3">
               <img src="./product_img/pic0102.jpg" class="card-img-top" alt="被束縛的貓 露芙娜">
               <div class="card-body">
@@ -341,27 +460,28 @@
               </div>
             </div> -->
           </div>
-          <div class="row mt-2">
-            <nav aria-label="Page navigation example">
-              <ul class="pagination justify-content-center">
-                <li class="page-item">
-                  <a class="page-link" href="#" aria-label="Previous">
-                    <span aria-hidden="true">&laquo;</span>
-                  </a>
-                </li>
-                <li class="page-item"><a class="page-link" href="#">1</a></li>
-                <li class="page-item active"><a class="page-link" href="#">2</a></li>
-                <li class="page-item"><a class="page-link" href="#">3</a></li>
-                <li class="page-item">
-                  <a class="page-link" href="#" aria-label="Next">
-                    <span aria-hidden="true">&raquo;</span>
-                  </a>
-                </li>
-              </ul>
-            </nav>
-          </div>
+          <!-- <div class="row mt-2">
+                  <nav aria-label="Page navigation example">
+                    <ul class="pagination justify-content-center">
+                      <li class="page-item">
+                        <a class="page-link" href="#" aria-label="Previous">
+                          <span aria-hidden="true">&laquo;</span>
+                        </a>
+                      </li>
+                      <li class="page-item"><a class="page-link" href="#">1</a></li>
+                      <li class="page-item active"><a class="page-link" href="#">2</a></li>
+                      <li class="page-item"><a class="page-link" href="#">3</a></li>
+                      <li class="page-item">
+                        <a class="page-link" href="#" aria-label="Next">
+                          <span aria-hidden="true">&raquo;</span>
+                        </a>
+                      </li>
+                    </ul>
+                  </nav>
+                </div> -->
         </div>
       </div>
+    </div>
   </section>
   <hr>
   <section id="scontent">
@@ -370,45 +490,45 @@
         <div class="col-md-2"><img src="./images/Qrcode02.png" alt="QRCODE" class="img-fluid mx-auto"></div>
         <div class="col-md-2">
           <i class="fas fa-thumbs-up fa-5x"></i>
-            <h3>關於我們</h3>
-            關於我們<br>
-            企業官網<br>
-            招商專區<br>
-            人才招募<br>
+          <h3>關於我們</h3>
+          關於我們<br>
+          企業官網<br>
+          招商專區<br>
+          人才招募<br>
         </div>
         <div class="col-md-2">
           <i class="fas fa-truck fa-5x"></i>
-            <h3>特色服務</h3>
-            特色服務<br>
-            大宗採購方案<br>
-            直配大陸<br>
+          <h3>特色服務</h3>
+          特色服務<br>
+          大宗採購方案<br>
+          直配大陸<br>
         </div>
         <div class="col-md-2">
           <i class="fas fa-users fa-5x"></i>
-            <h3>客戶服務</h3>
-            客戶服務<br>
-            訂單/配送進度查詢<br>
-            取消訂單/退貨<br>
-            更改配送地址<br>
-            追蹤清單<br>
-            12H速達服務<br>
-            折價券說明<br>
+          <h3>客戶服務</h3>
+          客戶服務<br>
+          訂單/配送進度查詢<br>
+          取消訂單/退貨<br>
+          更改配送地址<br>
+          追蹤清單<br>
+          12H速達服務<br>
+          折價券說明<br>
         </div>
         <div class="col-md-2">
           <i class="fas fa-comments-dollar fa-5x"></i>
-            <h3>好康大放送</h3>
-            好康大放送<br>
-            新會員禮包<br>
-            活動得獎名單<br>
+          <h3>好康大放送</h3>
+          好康大放送<br>
+          新會員禮包<br>
+          活動得獎名單<br>
         </div>
         <div class="col-md-2">
           <i class="fas fa-question fa-5x"></i>
-            <h3>FAQ 常見問題</h3>
-            FAQ 常見問題<br>
-            系統使用問題<br>
-            產品問題資詢<br>
-            大宗採購問題<br>
-            聯絡我們<br>
+          <h3>FAQ 常見問題</h3>
+          FAQ 常見問題<br>
+          系統使用問題<br>
+          產品問題資詢<br>
+          大宗採購問題<br>
+          聯絡我們<br>
         </div>
       </div>
     </div>
@@ -417,7 +537,7 @@
     <div class="container-fluid">
       <div id="last-data" class="row text-center">
         <div class="col-md-12">
-          <h6>中彰投分署科技股份有限公司 40767台中市西屯區工業區一路100號  電話：04-23592181 免付費電話：0800-777888</h6>
+          <h6>中彰投分署科技股份有限公司 40767台中市西屯區工業區一路100號 電話：04-23592181 免付費電話：0800-777888</h6>
           <h6>企業通過ISO/IEC27001認證，食品業者登錄字號：A-127360000-00000-0</h6>
           <h6>版權所有 copyright © 2017 WDA.com Inc. All Rights Reserved.</h6>
         </div>
